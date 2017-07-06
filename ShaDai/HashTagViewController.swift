04276f1c09d6a9a7946652f5aff8bb676227c5e5
lsylove <lsylove@sopooh90ui-MacBook-Air.local>
2017-07-06 16:00:26 +0900

    added HashTag highlighting feature
    - on the text field, entries starting with '#' are highlighted (blue)
    - hash tag identification is done through regular expression evaluation, which can be found at "String+.swift"

--- ShaDai/HashTagViewController.swift
+++ ShaDai/HashTagViewController.swift
@@ -13,22 +13,33 @@ class HashTagViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(HashTagViewController.cancelEdit))
+        
+        view.addGestureRecognizer(tap)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func cancelEdit() {
+        view.endEditing(true)
+    }
 
 }
-
 
 extension HashTagViewController: UITextViewDelegate {
 
     func textViewDidChange(_ textView: UITextView) {
         
-        print(textView.text)
+        let ranges = textView.text.hashTagRange
+        let attributedText = NSMutableAttributedString(string: textView.text)
+        
+        ranges.forEach{attributedText.addAttribute(NSForegroundColorAttributeName, value: UIColor.blue, range: $0)}
+        
+        textView.attributedText = attributedText
         
     }
 
