e16aeb5ce6057ecacbfa518ad0facd15725c7980
chicpark7 <chicpark7@golfzon.com>
2017-08-10 17:52:54 +0900

    add xcworkspace to use cocoapod which is dependency manager

---
+++ ShaDai/OpenCVWrapper.h
@@ -0,0 +1,17 @@
+//
+//  OpenCVWrapper.h
+//  ShaDai
+//
+//  Created by chicpark7 on 03/08/2017.
+//  Copyright © 2017 WebLinkTest. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OpenCVWrapper : NSObject
+
++ (UIImage*)detectFace:(UIImage*)image;
++ (UIImage*)detactSkeleton:(UIImage*)image;
++ (UIImage*)detactSkeleton:(UIImage*)image;
+
+@end
--- ShaDai/ShaDai-Bridging-Header.h
+++ ShaDai/ShaDai-Bridging-Header.h
@@ -2,4 +2,20 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+#import <Availability.h>
+
+#ifndef __IPHONE_5_0
+#warning "This project uses features only available in iOS SDK 5.0 and later."
+#endif
+
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+#endif
+
+#ifdef __cplusplus
+
+#endif
+
 #import "ObjC.h"
+#import "OpenCVWrapper.h"
